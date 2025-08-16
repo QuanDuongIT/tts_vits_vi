b1bb1bd1f823bfb5eaf97a3e315745b7b02a0386
Duong <tieuduong.25.1.98@gmail.com>
2025-08-16 09:50:25 +0700

    update text

--- text/symbols.py
+++ text/symbols.py
@@ -16,18 +16,8 @@
 # SPACE_ID = symbols.index(" ")
 from viphoneme import syms
 
-_pad = "_"
-_punctuation = ';:,.!?¡¿—…"«»“” '
-_letters = "AĂÂBCDĐEÊGHIKLMNOÔƠPQRSTUƯVXYZaăâbcdđeêghiklmnoôơpqrstuưvxy"
-
-excluded_chars = set(_pad + _punctuation + _letters)
-# Lọc những symbol KHÔNG nằm trong tập bị loại
-filtered_syms = [s for s in syms if s not in excluded_chars]
-
-_letters_ipa = ''.join(filtered_syms)
-
 # Export all symbols:
-symbols = [_pad] + list(_punctuation) + list(_letters) + list(_letters_ipa)
+symbols = ''.join(syms)
 
 # Special symbol ids
 SPACE_ID = symbols.index(" ")
